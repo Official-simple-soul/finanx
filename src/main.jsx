8346f058a884824002b6b7e56850546dd63eafbb
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2025-01-03 22:56:22 +0100

    feat: Implemented home page UI and auth UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import { BrowserRouter } from 'react-router';
-
-AOS.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
